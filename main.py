098fbc7bf946e2671c3b8efd9191f99c062e26c0
VitorBritoLaptop <vitorbrito1694@gmail.com>
2022-01-08 22:07:50 -0300

    Configuração de App e Organização de rotas

--- main.py
+++ main.py
@@ -1,45 +1,13 @@
 from fastapi import FastAPI, Depends, HTTPException
-from src.auth import AuthHandler
+from src.app.auth import AuthHandler
+from src.app.config import settings
 from src.models.schemas import AuthDetails
+from src.routes.api import api_router
 
+settings.PROJECT_NAME = "SigoAPI"
 
-app = FastAPI()
+app = FastAPI(
+    title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}"
+)
 
-
-auth_handler = AuthHandler()
-users = []
-
-@app.post('/register', status_code=201)
-def register(auth_details: AuthDetails):
-    if any(x['username'] == auth_details.username for x in users):
-        raise HTTPException(status_code=400, detail='Usuário Salvo com Sucesso!')
-    hashed_password = auth_handler.get_password_hash(auth_details.password)
-    users.append({
-        'username': auth_details.username,
-        'password': hashed_password    
-    })
-    return
-
-
-@app.post('/login')
-def login(auth_details: AuthDetails):
-    user = None
-    for x in users:
-        if x['username'] == auth_details.username:
-            user = x
-            break
-    
-    if (user is None) or (not auth_handler.verify_password(auth_details.password, user['password'])):
-        raise HTTPException(status_code=401, detail='Usuario ou Senha incorreto(a)')
-    token = auth_handler.encode_token(user['username'])
-    return { 'token': token }
-
-@app.get('/')
-@app.get('/unprotected')
-def unprotected():
-    return { 'hello': 'world' }
-
-
-@app.get('/protected')
-def protected(username=Depends(auth_handler.auth_wrapper)):
-    return { 'name': username }
+app.include_router(api_router, prefix=settings.API_V1_STR)
